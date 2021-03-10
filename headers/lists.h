ae90de8e3684b49de17464fb12243b209579b19f
dpanagea <sdi1400135@di.uoa.gr>
2021-03-10 12:59:36 +0200

    Created functions for list (node init, init list, add, print)

--- headers/lists.h
+++ headers/lists.h
@@ -3,14 +3,29 @@
 
 #include "functions.h"
 
-
-
 typedef struct listnode
 {
-    citizen value;
+    void* value;                /* we have different lists, no point in creating diff structs */
     struct listnode* previous;
     struct listnode* next;    
 }listnode;  
 
+typedef struct list
+{
+    int items;              /* Holds the num of items currently stored in the list */
+    struct listnode* head;
+    struct listnode* tail;
+}list;
+
+listnode* nodeInit(void*);
+list* listInit(listnode*);
+void listAdd(listnode*, list*);
+void listPrint(list*);
+
+int virusInList(char*, list*);                   /* check if virus already in list */
+struct record* recInList(int, list*, listnode*); /* check if record with citizen ID exists in list,
+                                                    starting from a certain node. Return the first found.
+                                                    No need to check in citizens' list, if there is a rec with
+                                                    same ID, then the citizen is already in the list */
 
 #endif
--- src/lists.c
+++ src/lists.c
@@ -1,2 +1,51 @@
 #include "../headers/lists.h"
 
+listnode* nodeInit(void* value)
+{
+    listnode* new = (listnode*)malloc(sizeof(listnode));
+    new->value = value;
+    new->next = NULL;
+    new->previous = NULL;
+
+    return new;
+}
+
+list* listInit(listnode* lnode)
+{
+    list* new = (list*)malloc(sizeof(list));
+    new->head = lnode;
+    new->tail = lnode;
+    new->items++;
+    
+    return new;
+}
+
+void listAdd(listnode* lnode, list* List) /* adds to the end of the list */
+{
+    if(List->head == NULL)
+    {
+        List->head = lnode;
+        List->tail = lnode;
+    }
+    else
+    {
+        List->tail->next = lnode;
+        lnode->previous = List->tail;
+        List->tail = lnode;
+    }
+    List->items++;
+}
+
+void listPrint(list* List)
+{
+    listnode* temp = List->head;
+    citizen* current;
+    while(temp != NULL && temp->value!=NULL)
+    {
+        current = temp->value;
+        printf("ID: %d | First name: %s | Last name: %s | Age: %d | Country: %s ", current->id, current->firstname, current->lastname, current->age, current->country);
+
+        temp = temp->next;
+    }
+
+}
