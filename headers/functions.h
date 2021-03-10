8c2462f6242a19a9d94698a023e15074009ea8e6
dpanagea <sdi1400135@di.uoa.gr>
2021-03-10 12:58:28 +0200

    Added basic structs (date, virus, citizen, rec)

--- headers/functions.h
+++ headers/functions.h
@@ -2,8 +2,24 @@
 #define _FUNCTIONS_
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "hashtable.h"
 #include "lists.h"
+
+typedef struct date
+{
+    int day;
+    int month;
+    int year;
+}date;
+
+typedef struct virus
+{
+    char* value;
+    void* skip_yes;
+    void* skip_no;
+    void* bloom;
+}virus;
 
 typedef struct citizen
 {
@@ -13,5 +29,18 @@ typedef struct citizen
     char* country;
     int age;
 }citizen;
+
+typedef struct record
+{
+    struct citizen* person;
+    struct virus* disease;
+    char vacc;
+    struct date vaccDate;
+}record;
+
+struct record* recordDef(char* buf); /*From buffer, create record */
+struct citizen* citizenDef(char* buf); /*From buffer, create citizen */
+struct virus* virusDef(char* buf); /*From buffer, create virus */
+struct date* dateDef(char* buf); /*From buffer, create date */
 
 #endif
